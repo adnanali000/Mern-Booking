9978f25bc167eab3c691f2e6a54ae74d3f8f2774
adnanali000 <adnan.museel123@gmail.com>
2022-05-23 14:16:42 +0500

    mail input done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/header/Header'
 import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/propertyList/PropertyList'
 import FeaturedProperties from '../../components/featuredProperty/FeaturedProperties'
+import MailList from '../../components/maillist/MailList'
 
 const Home = () => {
   return (
@@ -16,6 +17,7 @@ const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">Home guests love</h1>
         <FeaturedProperties />
+        <MailList />
       </div>
 
     </div>
