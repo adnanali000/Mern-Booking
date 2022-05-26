f18e88c1f13372dbb054ae760e9ac34a9571168b
adnanali000 <adnan.museel123@gmail.com>
2022-05-26 12:58:29 +0500

    footer work done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/propertyList/PropertyList'
 import FeaturedProperties from '../../components/featuredProperty/FeaturedProperties'
 import MailList from '../../components/maillist/MailList'
+import Footer from '../../components/footer/Footer'
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
         <h1 className="homeTitle">Home guests love</h1>
         <FeaturedProperties />
         <MailList />
+        <Footer />
       </div>
 
     </div>
